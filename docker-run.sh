d5065fca9cbd72385c1fd4cf5b90373d1ee948fb
zhuratop <nezhuravlev@yandex.ru>
2025-05-15 00:02:25 +0300

    docker-run

--- docker-run.sh
+++ docker-run.sh
@@ -4,4 +4,4 @@
 docker build -t pneumonia-app .
 
 # Запускаем контейнер
-docker run -d --name pneumonia-container-app pneumonia-app git
+docker run -d -p 5000:5000 --name pneumonia-container-app pneumonia-app
